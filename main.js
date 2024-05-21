37500bf33be8fa6a3de2d1c40763dbe34e7be5ea
Brinkslu991 <brinkslu991@northwested.org>
2024-05-21 10:54:21 -0400

    gehwryiy

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-// Student name
-// Current date
+// Lucas Brinks
+// 5/21/2024
 // JS IF Statements
 
+const credits=120;
+const creditsNow = parseInt(prompt('Please enter your current credits'));
+if(creditsNow >= (credits)){
+    alert("you somehow did it");
+} else if (creditsNow >= 90){
+    alert("you're close");
+} else {
+    alert("you failed");
+}
+    
